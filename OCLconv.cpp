507933126ddc491cd847694a72429044ce0a72f9
Murivan <Murivan@users.noreply.github.com>
2013-03-06 14:28:15 +0000

    Fix to typedef in OCLconv.cpp

--- OCLconv.cpp
+++ OCLconv.cpp
@@ -272,7 +272,7 @@ const char* cSourceFile = "OCLComplexPwiseMul.cl";
 cl_device_id     device_id;
 cl_context       context;
 cl_command_queue queue;
-typedef unsigned long long ulong;
+    //typedef unsigned long long ulong;
 
 
 
